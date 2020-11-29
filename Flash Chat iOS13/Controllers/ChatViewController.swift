ecfe2426f0f94a774576fba5afc417d99b56148a
Rodrigo <rodrigo@Rodrigos-MacBook-Pro.local>
2020-11-28 23:49:52 -0500

    first commit

---
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ChatViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    var message: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        title = K.appName
+        navigationItem.hidesBackButton = true
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+          try Auth.auth().signOut()
+          navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print ("Error signing out: %@", signOutError)
+        }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return message.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = message[indexPath.row].body
+        return cell
+    }
+}
+
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+    //
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Rodrigo on 11/5/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+    
+struct Message {
+    let sender: String
+    let body: String
+}
